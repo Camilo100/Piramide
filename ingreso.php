066fbe9144aec957841736a39d6825649df22e4d
Fela <felipemirande@gmail.com>
2015-07-07 13:56:42 -0300

    correcion de ingreso

--- ingreso.php
+++ ingreso.php
@@ -31,51 +31,18 @@
 					  	<input type="int" placeholder = "14" name = "14" class="form-control" >
 							<input type="int" placeholder = "15" name = "15" class="form-control" ></div><br>
 						<div class = "center-block6">
-              <!DOCTYPE html>2
-              <html>
-              <link rel="stylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap.css">
-              <body>
-              <div class="container">
-                  <div class="row">
-                  	<div class="span6">
-                         <div class="panel panel-default">
-                              <div class="panel-heading">
-                                  <span class="glyphicon"></span> Piramide</div>
-                              <div class="panel-body">
-                              	<form action = "piramide.php">
-              						<div class = "center-block1">
-              							<input type="int" placeholder = "1" class="form-control" name = "1" ></div><br>
-              						<div class = "center-block2">
-              			    		<input type="int" placeholder = "2" class="form-control" name = "2" >
-                    				<input type="int" placeholder = "3" name = "3" class="form-control" ></div><br>
-              				  	<div class = "center-block3">
-              					  	<input type="int" placeholder = "4" name = "4" class="form-control">
-              					  	<input type="int" placeholder = "5" name = "5" class="form-control" >
-                          	<input type="int" placeholder = "6" name = "6" class="form-control" ></div><br>
-              				  	<div class = "center-block4">
-              							<input type="int" placeholder = "7" name = "7" class="form-control" >
-                          	<input type="int" placeholder = "8" name = "8" class="form-control" >
-              							<input type="int" placeholder = "9" name = "9" class="form-control" >
-              							<input type="int" placeholder = "10" name = "10" class="form-control" ></div><br>
-              						<div class = "center-block5">
-              							<input type="int" placeholder = "11" name = "11" class="form-control" >
-                            <input type="int" placeholder = "12" name = "12" class="form-control" >
-              					 		<input type="int" placeholder = "13" name = "13" class="form-control" >
-              					  	<input type="int" placeholder = "14" name = "14" class="form-control" >
-              							<input type="int" placeholder = "15" name = "15" class="form-control" ></div><br>
-              						<div class = "center-block6">
-              							<input type="int" placeholder = "16" name = "16" class="form-control">
-              							<input type="int" placeholder = "17" name = "17" class="form-control">
-              							<input type="int" placeholder = "18" name = "18" class="form-control">
-              							<input type="int" placeholder = "19" name = "19" class="form-control">
-              							<input type="int" placeholder = "20" name = "20" class="form-control">
-              							<input type="int" placeholder = "21" name = "21" class="form-control"></div><br>
-              </div>
-              </div>
-              </div>
-              </div>
-              </div>
+							<input type="int" placeholder = "16" name = "16" class="form-control">
+							<input type="int" placeholder = "17" name = "17" class="form-control">
+							<input type="int" placeholder = "18" name = "18" class="form-control">
+							<input type="int" placeholder = "19" name = "19" class="form-control">
+							<input type="int" placeholder = "20" name = "20" class="form-control">
+							<input type="int" placeholder = "21" name = "21" class="form-control"></div><br>
+</div>
+</div>
+</div>
+</div>
+</div>
 
-              </form>
-              </body>
-              </html>
+</form>
+</body>
+</html>
